b6d27bed59f1dce4e6d713a1f14ac84bf48c9f34
Tchetche Moise <tchetchemoise@MacBook-Pro-de-Tchetche.local>
2026-02-01 18:52:55 +0100

    mise en place de la configuration test pour voir si les API fonctionne version2

--- frontend/real-time-chat/app/test/page.tsx
+++ frontend/real-time-chat/app/test/page.tsx
@@ -16,9 +16,6 @@ export default async function TestPage() {
       <div style={{ padding: 20 }}>
         <h1>TEST API : KO ❌</h1>
         <p>{errorMessage}</p>
-        <p>
-          👉 C’est NORMAL si tu n’es pas encore connecté.
-        </p>
       </div>
     );
   }
@@ -26,7 +23,6 @@ export default async function TestPage() {
   return (
     <div style={{ padding: 20 }}>
       <h1>TEST API : OK ✅</h1>
-      <p>Le frontend arrive à parler au backend.</p>
       <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
